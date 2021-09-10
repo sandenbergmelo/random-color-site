da685071a8f20ff7c316ae10c38474614779e699
Sandenberg <sandenbergferreiramelo@gmail.com>
2021-09-10 10:50:34 -0300

    Change var and let to const

--- js/random.js
+++ js/random.js
@@ -1,4 +1,4 @@
-var btn = document.getElementById('btn') // Button object
+const btn = document.getElementById('btn') // Button object
 
 // Events that will trigger the functions
 window.addEventListener('load', randomColor)
@@ -6,11 +6,9 @@ btn.addEventListener('click', randomColor)
 
 function randomColor() {// Function to change the background to a random color
 	
-	let r, g, b
-
-	r = Math.floor(Math.random() * 256)
-	g = Math.floor(Math.random() * 256)
-	b = Math.floor(Math.random() * 256)
+	const r = Math.floor(Math.random() * 256)
+	const g = Math.floor(Math.random() * 256)
+	const b = Math.floor(Math.random() * 256)
 
 	document.body.style.background = `rgb(${r}, ${g}, ${b})`
 
